11f9c3ef7bccb4371a9938a14d311eadb0058feb
David Dudas <david.dudas@borndigital.ai>
2024-03-13 10:06:07 +0100

    improvements to support voice channel

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import json
 import copy
-
+import requests
 
 from fastapi import FastAPI, Query
 from openai import OpenAI
@@ -21,7 +21,7 @@ client = OpenAI(timeout=10.0, max_retries=2)
 
 
 DEFAULT_MSG = [
-    {"role": "system", "content": "You are a Bakery Salesman. Help user buy bakery goods. Introduce yourself and ask user how you can help. Komunikuj v češtině."},
+    {"role": "system", "content": "You are a Bakery Salesman John. Help user buy bakery goods. Briefly introduce yourself and ask user how you can help. Komunikuj v češtině."},
 ]
 
 IN_MEM_DATA = {}
@@ -49,8 +49,42 @@ async def get_conversation_user(CID: str, text: str = Query(None)):
         return str(cart)
 
     def checkout() -> str:
-        """Confirms order from a client"""
+        """Confirms order and sents confirmation sms with a cart content to user phone number"""
+#        logging.info(f"sending sms to {phone_number}")#
+#
+#        if phone_number is None:
+#            logging.info("Number is None")
+#            return "We need a 9 digit phone number."#
+#
+#        if len(phone_number) != 9 or phone_number == "123456789":
+#            logging.info("Number is not valid")
+#            return "phone number should be valid 9 digits long. Supporting only czech numbers without international prefix."#
+#
+#        # Define the URL
+#        url = "https://http-api.smsmanager.cz/Send"#
+#
+#        # Specify the payload as a dictionary
+#        payload = {
+#            'apikey': 'xxx',
+#            'number': f'420{phone_number}',
+#            'message': message
+#        }#
+#
+#        # Make the POST request
+#        response = requests.post(url, data=payload)
         logging.info("Confirming the order")
+        return "Done"
+
+    def check_availability(item_name: str) -> str:
+        """Check if the item is available"""
+        logging.info("Checking availability")
+        if item_name in available_items:
+            logging.info("Item aiavilable")
+            return "Item is available"
+        else:
+            items = str([item.page_content for item in available_items_db.similarity_search(item_name)][:3])
+            logging.info(f"Found options for a query {items}")
+            return f"Wrong item name. Give user a choice if it is not clear what he wants: {items}"    
         return "Done"
 
     def add_item_to_cart(item_name: str, count: int) -> str:
@@ -61,14 +95,15 @@ async def get_conversation_user(CID: str, text: str = Query(None)):
             logging.info("Item added to the cart")
             return "Item added to the cart"
         else:
-            items = str([item.page_content for item in available_items_db.similarity_search(item_name)])
+            items = str([item.page_content for item in available_items_db.similarity_search(item_name)][:3])
             logging.info(f"Found options for a query {items}")
-            return f"Wrong item name. We can offer these similar items: {items}"    
+            return f"Wrong item name. Give user a choice if it is not clear what he wants. Otherwise add corect item to the basket. Options: {items}"    
 
     available_functions = {
         "add_item_to_cart": add_item_to_cart,
         "checkout": checkout,
-        "get_cart_items": get_cart_items
+        "get_cart_items": get_cart_items,
+        "check_availability": check_availability
     }  
 
     counter = 0
@@ -84,7 +119,7 @@ async def get_conversation_user(CID: str, text: str = Query(None)):
         response = client.chat.completions.create(
             model="gpt-3.5-turbo-0125",
             messages=messages,
-            tools=[get_openai_func_def(get_cart_items), get_openai_func_def(add_item_to_cart), get_openai_func_def(checkout)],
+            tools=[get_openai_func_def(get_cart_items), get_openai_func_def(add_item_to_cart), get_openai_func_def(checkout), get_openai_func_def(check_availability)],
         )
         response_message = response.choices[0].message
         logging.info(f"\n Response messages: {response_message}")
@@ -110,4 +145,4 @@ async def get_conversation_user(CID: str, text: str = Query(None)):
             IN_MEM_DATA[CID] = {"cart": cart, "messages": messages}
             logging.info(f"\n GPT message: {response_message.content}")
             return {"text": response_message.content}
-            
+        
--- vector_db.py
+++ vector_db.py
@@ -5,24 +5,24 @@ from langchain_community.vectorstores import FAISS
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
 available_items = [
-    "Mazanec 400g",
-    "Mazanec s rozinkami sypaný mandlemi 400g",
-    "Mazanec máslový 400g",
-    "Vánočka s rozinkami 200g",
-    "Vánočka 400g",
-    "Vánočka máslová rozinková manandlová 600g",
-    "Vánočka s rozinkami a mandlemi 5 ks á 400g 2000g",
-    "Špička s náplní makovou 2ks á 110g",
-    "Špička s náplní makovou a tvarohovou 2ks á110g",
-    "Chléb konzumní s kmínem 1200g",
-    "Chléb konzumní s kmínem 500g - předpečený BIDVEST",
-    "Chléb s kmínem Šumava 6x570g 3420gBK",
-    "Zelňák 30g - předpečený",
-    "Rohlík 43g",
-    "Raženka bez posyp. 30x60g",
-    "Bábovky MIX (3ks mramorová 400g, 3ks šlehaná 400g) 2400g",
-    "Klimentovský chléb (jedlá etiketa) 800g",
-    "Tesco Finest Vícezrnný chléb s quinoou 500g"
+    "Mazanec 400 gramů",
+    "Mazanec s rozinkami sypaný mandlemi 400 gramů",
+    "Mazanec máslový 400 gramů",
+    "Vánočka s rozinkami 200 gramů",
+    "Vánočka 400 gramů",
+    "Vánočka máslová rozinková manandlová 600 gramů",
+    "Vánočka s rozinkami a mandlemi 5 kůsů po 400 gramech",
+    "Špička s náplní makovou 2 kusy po 110 gramech",
+    "Špička s náplní makovou a tvarohovou 2 kusy po 110 gramech",
+    "Chléb konzumní s kmínem 1200 gramů",
+    "Chléb konzumní s kmínem 500 gramů",
+    "Chléb s kmínem Šumava 6 krát 570 gramů",
+    "Zelňák 30 gramů - předpečený",
+    "Rohlík 43 gramů",
+    "Raženka bez posyp. 30 krát 60 gramů",
+    "Bábovky MIX 2400 gramů",
+    "Klimentovský chléb - jedlá etiketa 800 gramů",
+    "Tesco Finest Vícezrnný chléb s quinoou 500 gramů"
 ]
 
 available_items_db = FAISS.from_texts(available_items, embeddings)
